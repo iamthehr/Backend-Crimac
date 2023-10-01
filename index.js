3fed5247e37c259af70fb58ece5e30cb6f6669da
iamthehr <harshraj5565@gmail.com>
2023-10-01 23:01:46 +0530

    minor

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use("/api/user", require("./routes/authRoutes"));
 app.post("/api/verifyToken", verifyToken);
 app.use("/api/search", tokenCheck, searchRouter);
+app.use("/api/upload", tokenCheck, require("./routes/uploadRoutes"));
 
 app.listen(3080, () => {
   console.log("Server is running on port 3080");
